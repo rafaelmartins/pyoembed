bf3497f735470df21d751f2aa6e57bcef0485609
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-03-03 18:00:02 -0300

    fix pyflakes warning

--- pyoembed/tests/parsers/test_json_parser.py
+++ pyoembed/tests/parsers/test_json_parser.py
@@ -14,7 +14,8 @@ class JsonParserTestCase(unittest.TestCase):
         self.assertTrue(self.parser.content_supported('text/json'))
         self.assertTrue(self.parser.content_supported('application/json'))
         self.assertTrue(self.parser.content_supported('text/javascript'))
-        self.assertTrue(self.parser.content_supported('application/javascript'))
+        self.assertTrue(
+            self.parser.content_supported('application/javascript'))
         self.assertFalse(self.parser.content_supported('text/xml'))
 
     def test_content_parse(self):
