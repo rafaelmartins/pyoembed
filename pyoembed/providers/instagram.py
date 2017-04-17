d6d34a6a73404ef6bffe79109da68feb224ef8c6
Chad Paulson <chad.paulson@gmail.com>
2017-04-16 22:07:10 -0500

    Updates InstagramProvider schemas and endpoint in order to support https.

--- pyoembed/providers/instagram.py
+++ pyoembed/providers/instagram.py
@@ -4,6 +4,8 @@ from pyoembed.providers import BaseProvider
 class InstagramProvider(BaseProvider):
 
     priority = 10
-    oembed_schemas = ['http://instagram.com/p/*',
+    oembed_schemas = ['https://instagram.com/p/*',
+                      'https://instagr.am/p/*',
+                      'http://instagram.com/p/*',
                       'http://instagr.am/p/*']
-    oembed_endpoint = 'http://api.instagram.com/oembed'
+    oembed_endpoint = 'https://api.instagram.com/oembed'
