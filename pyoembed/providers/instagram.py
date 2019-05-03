d37d041c331f960e11aac25db8069be166a0a5f4
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-05-03 09:17:00 +0530

    instagram: Allow www in instagram oembed schema urls.

--- pyoembed/providers/instagram.py
+++ pyoembed/providers/instagram.py
@@ -6,6 +6,11 @@ class InstagramProvider(BaseProvider):
     priority = 10
     oembed_schemas = ['https://instagram.com/p/*',
                       'https://instagr.am/p/*',
+                      'https://www.instagram.com/p/*',
+                      'https://www.instagr.am/p/*'
                       'http://instagram.com/p/*',
-                      'http://instagr.am/p/*']
+                      'http://instagr.am/p/*',
+                      'http://www.instagram.com/p/*',
+                      'http://www.instagr.am/p/*']
+
     oembed_endpoint = 'https://api.instagram.com/oembed'
